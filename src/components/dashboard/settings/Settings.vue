cf95dade38fe0b2e6fdd93745f13c586d58e3527
Mhmod-hsn <hsn.mhmod.96@gmail.com>
2020-05-02 23:06:18 +0200

    done settings

---
+++ src/components/dashboard/settings/Settings.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="settings">
+    <h2>Settings</h2>
+    <v-card>
+      <v-container>
+        <!--				Shop Name -->
+        <v-row>
+          <v-col>
+            <v-text-field label="Sop Name" hide-details="auto" v-model="settings.shopName"></v-text-field>
+          </v-col>
+        </v-row>
+
+        <!--      Currency -->
+        <v-row>
+          <v-col>
+            <v-text-field label="Currency" v-model="settings.currency"></v-text-field>
+          </v-col>
+        </v-row>
+
+        <!--				Dark Theme -->
+        <v-row>
+          <v-col>
+            <v-switch label="Dark Theme" labe v-model="settings.darkTheme" color="red"></v-switch>
+          </v-col>
+        </v-row>
+
+        <v-btn class="ml-auto d-block" color="primary" @click="saveSettings">Save</v-btn>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Settings",
+  data() {
+    return {};
+  },
+  methods: {
+    saveSettings() {
+      this.$store.dispatch("updateSettings", this.settings);
+    }
+  },
+  computed: {
+    settings() {
+      return this.$store.state.settings.settings;
+    }
+  },
+  mounted() {
+    this.$store.dispatch("getSettings");
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/store/settings.js
@@ -0,0 +1,91 @@
+import db from '@/firebase/init'
+
+export default {
+  state: {
+    categories: []
+  },
+  actions: {
+    addCategory({commit}, {category}) {
+      db.collection('category').add({
+        name: category.name,
+        slug: category.slug
+      }).then(() => {
+        commit('ADD_CATEGORY', category)
+      }).catch(e => {
+        console.log('error' + e)
+      })
+    },
+    getCategories({commit}) {
+      let cats = [];
+      // fetch data from firestore
+      db.collection('category').get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            let c = doc.data();
+            c.id = doc.id;
+            cats.push(c)
+          })
+        }).then(() => {
+        commit('SET_CATEGORIES', cats)
+      }).catch(e => {
+        console.log('error' + e)
+      })
+
+    },
+    editCategory({commit}, category) {
+      db.collection('category').doc(category.id).update({
+        name: category.name,
+        slug: category.slug
+      })
+        .then(() => {
+          commit('EDIT_CATEGORY', category)
+
+        }).catch(e => {
+        console.log('error' + e)
+      })
+    },
+    removeCategory({commit}, category) {
+      db.collection('category').doc(category.id).delete()
+        .then(() => {
+          commit('REMOVE_CATEGORY', category)
+        }).catch(e => {
+        console.log('error' + e)
+      })
+    }
+  },
+  mutations: {
+    SET_CATEGORIES(state, categories) {
+      state.categories = categories
+    },
+
+    ADD_CATEGORY(state, category) {
+      state.categories.push(category)
+    },
+
+    REMOVE_CATEGORY(state, category) {
+      state.categories = state.categories.filter(cat => {
+        return cat.id !== category.id
+      })
+
+      // let index = null
+      // for (let i = 0; i < state.categories.length; i++) {
+      //   if (state.categories[i].id === category.id) {
+      //     index = i;
+      //     break
+      //   }
+      // }
+      // state.categories.splice(index, 1)
+    },
+    EDIT_CATEGORY(state, category) {
+      let index = null;
+      for (let i = 0; i < state.categories.length; i++) {
+        if (state.categories[i].id === category.id) {
+          state.categories[i].name = category.name;
+          state.categories[i].slug = category.slug;
+          break
+        }
+      }
+    }
+  },
+  getters: {},
+}
